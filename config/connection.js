a7e17c687641deaeda571d9707d9b16078c3b2e0
NickM <nmikstas@alumni.scu.edu>
2019-11-18 17:18:33 -0700

    Added Heroku database support

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,28 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+let mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection(
+if(process.env.JAWSDB_URL)
 {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+    connection = mysql.createConnection(
+    {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err)
 {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
